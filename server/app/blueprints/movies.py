af704c157c488866c77392ea3cc02d14a2a7cade
KelvinQiu <kelvinqiu802@outlook.com>
2023-10-22 22:01:16 +0800

    feat: get the number of movies

--- server/app/blueprints/movies.py
+++ server/app/blueprints/movies.py
@@ -8,3 +8,9 @@ movies = Blueprint('movies', __name__)
 def get_movie(id) -> Response:
     movie: Movies = Movies.query.get(id)
     return jsonify(movie.to_dict())
+
+
+@movies.get('/count')
+def get_count() -> Response:
+    count = Movies.query.count()
+    return jsonify({"count": count})
