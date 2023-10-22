81f111c0cb5807677f58ff6182de24d94be4c565
KelvinQiu <kelvinqiu802@outlook.com>
2023-10-22 21:39:38 +0800

    feat: use remote db

--- server/app/db/db_config.py
+++ server/app/db/db_config.py
@@ -1,6 +1,15 @@
+# Localhost
+# dialect = 'mysql'
+# user_name = 'root'
+# password = ''
+# host = 'localhost'
+# port = '3306'
+# database = 'flask'
+
+# Remote
 dialect = 'mysql'
-user_name = 'root'
-password = ''
-host = 'localhost'
+user_name = 'douban'
+password = 'NZpYSjmcKm35mfin'
+host = '81.70.146.18'
 port = '3306'
-database = 'flask'
+database = 'douban'
