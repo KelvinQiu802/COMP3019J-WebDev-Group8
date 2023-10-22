c9d1bc992f65cf67e8e3b868367fcd540c942299
KelvinQiu <kelvinqiu802@outlook.com>
2023-10-22 21:26:31 +0800

    feat: add bookmark page

---
+++ client/src/app/bookmark/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import style from './page.module.css';
+
+function page() {
+  return (
+    <div>
+      <h2 className={style.center}>Render the bookmarks in this page.</h2>
+    </div>
+  );
+}
+
+export default page;
