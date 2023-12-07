4bb92c4e25998463f2d16437d14a7503dd129bbe
KelvinQiu <kelvinqiu802@outlook.com>
2023-12-07 10:05:26 +0800

    feat: admin tabs

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -1,12 +1,29 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import style from './page.module.css';
 import API_HOST from '../../../utils/host';
 import { useRouter } from 'next/navigation';
+import { Box, Tab, Tabs } from '@mui/material';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div hidden={value !== index} {...other} className={style.manage}>
+      {value === index && <div>{children}</div>}
+    </div>
+  );
+}
 
 function Page() {
   const router = useRouter();
+  const [tabIndex, setTabIndex] = useState(0);
 
   // Auth
   useEffect(() => {
@@ -17,13 +34,31 @@ function Page() {
       }
       const response = await fetch(`${API_HOST}/users/auth/${userName}`);
       if (response.status == 401) {
-        console.log('HAHAHAH');
         router.push('/unauthorized');
       }
     })();
   }, []);
 
-  return <div>page</div>;
+  return (
+    <div>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={tabIndex} onChange={(_, val) => setTabIndex(val)} centered>
+          <Tab label="Accounts" value={0} />
+          <Tab label="Movies" value={1} />
+          <Tab label="Others" value={2} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={tabIndex} index={0}>
+        Item One
+      </CustomTabPanel>
+      <CustomTabPanel value={tabIndex} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={tabIndex} index={2}>
+        Item Three
+      </CustomTabPanel>
+    </div>
+  );
 }
 
 export default Page;
