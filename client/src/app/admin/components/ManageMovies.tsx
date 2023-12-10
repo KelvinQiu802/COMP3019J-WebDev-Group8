7cbc93d274e3b911f2fbc4cce5754461f88eef0f
KelvinQiu <kelvinqiu802@outlook.com>
2023-12-10 13:08:38 +0800

    feat: remove movie func

--- client/src/app/admin/components/ManageMovies.tsx
+++ client/src/app/admin/components/ManageMovies.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState, ChangeEvent } from 'react';
 import style from './movies.module.css';
 import API_HOST from '../../../../utils/host';
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
-import { Pagination } from '@mui/material';
+import { Button, Pagination } from '@mui/material';
+import { ToastContainer, toast } from 'react-toastify';
 
-const LIMIT = 10;
+const LIMIT = 15;
 
 async function getPageCount() {
   const result = await fetch(`${API_HOST}/movies/count`);
@@ -23,9 +24,13 @@ function ManageMovies() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [pageCount, setPageCount] = useState(1);
   const [page, setPage] = useState(1);
+  const [update, setUpdate] = useState(true);
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathName = usePathname();
+
+  const notifyDeleted = (userName: string) =>
+    toast(`Movie: ${userName} Deleted!`);
 
   function handlePageChange(e: ChangeEvent<unknown>, pageNum: number) {
     router.push(`${pathName}?page=${pageNum}`);
@@ -35,7 +40,7 @@ function ManageMovies() {
     (async () => {
       setPageCount(await getPageCount());
     })();
-  }, []);
+  }, [update]);
 
   useEffect(() => {
     (async () => {
@@ -43,12 +48,38 @@ function ManageMovies() {
       setPage(page ? page : 1);
       setMovies(await getMovies(page ? page : 1, LIMIT));
     })();
-  }, [searchParams]);
+  }, [searchParams, update]);
+
+  const handleRemove = async (id: number, name: string) => {
+    await fetch(`${API_HOST}/movies/${id}`, { method: 'DELETE' });
+    setUpdate((prev) => !prev);
+    notifyDeleted(name);
+  };
+
+  const handleEdit = (id: number) => {};
 
   return (
     <div className={style.moviesList}>
       {movies.map((m) => (
-        <div key={m.movie_id}>{m.movie_title}</div>
+        <div key={m.movie_id} className={style.movie}>
+          <div>{m.movie_title}</div>
+          <div className={style.btns}>
+            <Button
+              onClick={() => handleRemove(m.movie_id, m.movie_title)}
+              variant="contained"
+              color="error"
+            >
+              DEL
+            </Button>
+            <Button
+              onClick={() => handleEdit(m.movie_id)}
+              variant="contained"
+              color="warning"
+            >
+              EDIT
+            </Button>
+          </div>
+        </div>
       ))}
       <Pagination
         count={pageCount}
@@ -56,6 +87,18 @@ function ManageMovies() {
         className={style.pagination}
         page={page}
         onChange={handlePageChange}
+      />
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
       />
     </div>
   );
